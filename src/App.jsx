a6e1a4c220c8cb5662d963447dbd348dd5f0250b
Harshavardhan Reddy M <harshavardhanreddy0406@gmail.com>
2025-05-04 15:31:09 +0530

    updated RESUME

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Skills from './components/Skills/Skills'
 import Projects from './components/Projects/Projects'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App() {
   return (
